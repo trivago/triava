71f4dbdc4c6f791817f70b3397fe2553612d93f2
Christian Esken <Christian.Esken@trivago.com>
2015-09-29 11:27:10 +0200

    SRT-20268 Support all time units in time(). And indentation.

--- src/main/java/com/trivago/triava/time/EstimatorTimeSource.java
+++ src/main/java/com/trivago/triava/time/EstimatorTimeSource.java
@@ -6,20 +6,20 @@ import com.trivago.triava.logging.TriavaLogger;
 import com.trivago.triava.logging.TriavaNullLogger;
 
 /**
- * A TimeSource that provides the value of System.currentTimeMillis() in 10ms precision. 
- * Target use case for this class is to reduce overhead of time methods like System.currentTimeMillis() or
- * System.nanoTime(). like thousand or million times per second.
+ * A TimeSource that provides the value of System.currentTimeMillis() in the given millisecond precision.
+ * Target use case for this class is to reduce the overhead of System.currentTimeMillis() or
+ * System.nanoTime(), when being called very frequently (thousands or million times per second).
  * <p>
- * Short story: It increases the write throughput to the Cache massively (100% or even 1000%).
+ * This implementation uses a background thread to regularly fetch the current time from
+ * System.currentTimeMillis(). The value is cached and returned in all methods returning a time. Caching is
+ * helpful, as fetching the time includes a potentially expensive crossing of the operating system boundary
+ * (calling native, levering privilege, register swapping, ...). If you call it thousands or millions of times
+ * per second, this has a very noticeable effect. For these cases the throughput can be massively increased
+ * (100% or even 1000%).
  * <p>
- * Details: The implementation implements "read-once", "provide-often": The value is fetched from the
- * operating system once by a background thread. Fetching includes a potentially expensive crossing of the
- * operating system boundary (calling native, levering privilege, register swapping, ...). If you call it
- * thousands or millions of times per second, this has a very noticeable effect. Regarding the correctness
- * this means that more writes are done with the same timestamp. For expiration this does not have any side
- * effect. For eviction it only effects LRU - more entries would be possible eviction targets - but within
- * 10ms it is usually not relevant which entry is evicted first.
- * 
+ * Users of this class must be aware that all calls within the update interval will return exactly the same
+ * time. For example a Cache implementation that uses this class will have more entries with the identical
+ * incoming timestamp. This can have an effect on expiration or time-based eviction strategies like LRU.
  * 
  * @author cesken
  *
@@ -28,36 +28,41 @@ public class EstimatorTimeSource extends Thread implements TimeSource
 {
 	final int UPDATE_INTERVAL_MS;
 	private volatile long millisEstimate = System.currentTimeMillis();
-	private volatile long secondsEstimate = millisEstimate /1000;
+	private volatile long secondsEstimate = millisEstimate / 1000;
 
-	private volatile boolean running;  // Must be volatile, as it is modified via cancel() from a different thread
+	private volatile boolean running; // volatile. Modified via cancel() from a different thread
 	private final TriavaLogger logger;
 
 	/**
 	 * Creates a TimeSource with the given update interval.
-	 * @param updateIntervalMillis Update interval, which defines the approximate precision
+	 * 
+	 * @param updateIntervalMillis
+	 *            Update interval, which defines the approximate precision
 	 */
 	public EstimatorTimeSource(int updateIntervalMillis)
 	{
 		this(updateIntervalMillis, new TriavaNullLogger());
 	}
-	
+
 	/**
 	 * Creates a TimeSource with the given update interval.
-	 * @param updateIntervalMillis Update interval, which defines the approximate precision
-	 * @param logger A non-null triava logger
+	 * 
+	 * @param updateIntervalMillis
+	 *            Update interval, which defines the approximate precision
+	 * @param logger
+	 *            A non-null triava logger
 	 */
 	public EstimatorTimeSource(int updateIntervalMillis, TriavaLogger logger)
 	{
 		this.logger = logger;
 		this.UPDATE_INTERVAL_MS = updateIntervalMillis;
 		setName("MillisEstimatorThread-" + UPDATE_INTERVAL_MS + "ms");
-	    setPriority(Thread.MAX_PRIORITY);
-	    setDaemon(true);
-	    start();
+		setPriority(Thread.MAX_PRIORITY);
+		setDaemon(true);
+		start();
 
 	}
-	
+
 	public void run()
 	{
 		logger.info("MillisEstimatorThread " + this.getName() + " has entered run()");
@@ -69,7 +74,6 @@ public class EstimatorTimeSource extends Thread implements TimeSource
 				sleep(UPDATE_INTERVAL_MS);
 				millisEstimate = System.currentTimeMillis();
 				secondsEstimate = millisEstimate / 1000;
-//				System.out.println("seconds=" + secondsEstimate);
 			}
 			catch (InterruptedException ex)
 			{
@@ -88,13 +92,18 @@ public class EstimatorTimeSource extends Thread implements TimeSource
 	public void shutdown()
 	{
 		cancel();
-		
+
 	}
 
 	@Override
 	public long time(TimeUnit tu)
 	{
-		throw new UnsupportedOperationException("time() is not implemented by TimeSource: MillisEstimatorThread");
+		if (tu == TimeUnit.SECONDS)
+			return secondsEstimate;
+		else if (tu == TimeUnit.MILLISECONDS)
+			return millisEstimate;
+		else
+			return tu.convert(millisEstimate, TimeUnit.MILLISECONDS);
 	}
 
 	@Override
@@ -110,4 +119,3 @@ public class EstimatorTimeSource extends Thread implements TimeSource
 	}
 
 }
-
