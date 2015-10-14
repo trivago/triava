c28daafd973cc3526dd0a5feec02f7df4fe6d4bb
Christian Esken <Christian.Esken@trivago.com>
2015-10-14 14:53:43 +0200

    SRT-20268 Add Apache v2 license headers

--- src/main/java/com/trivago/triava/annotations/Alpha.java
+++ src/main/java/com/trivago/triava/annotations/Alpha.java
@@ -1,7 +1,23 @@
+/*********************************************************************************
+ * Copyright 2015-present trivago GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************************/
+
 package com.trivago.triava.annotations;
 
 /**
- * Classes or methods marked with this Annotation indicate that this API is not stable, and may change anytime,
+ * Classes or methods marked with this Annotation indicate that this API is not stable, and may change any time,
  * even within minor versions.
  * 
  * @author cesken
