d77571f0fd02eac0cd2df0fe67bffc7f37c99f00
Christian Esken <Christian.Esken@trivago.com>
2016-01-20 14:25:49 +0100

    SRT-28657 Documentation

--- src/main/java/com/trivago/triava/tcache/core/StorageBackend.java
+++ src/main/java/com/trivago/triava/tcache/core/StorageBackend.java
@@ -32,7 +32,7 @@ public interface StorageBackend<K,V>
 	/**
 	 * Returns a ConcurrentMap conforming to the configuration specified in the builder.
 	 * The sizeFactor is a hint that the Map should be sized bigger due to the Cache implementation. For example
-	 * if eviction starts at 95% fill rate, sizeFactor could be 1/0.95 = 1,0526316 
+	 * if eviction starts at 95% fill rate, sizeFactor could be 1 - 1/0.95 = 0,0526316 
 	 * 
 	 * @param builder The configuration parameter for the Map
 	 * @param sizeFactor Sizing factor to take into account
