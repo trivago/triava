def647a7dd68f95a8fe70f51965ea929450d2e45
Christian Esken <Christian.Esken@trivago.com>
2016-05-24 17:37:35 +0200

    SAE-19 Add license headers

--- src/main/java/com/trivago/triava/tcache/action/PutAction.java
+++ src/main/java/com/trivago/triava/tcache/action/PutAction.java
@@ -1,3 +1,19 @@
+/*********************************************************************************
+ * Copyright 2016-present trivago GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************************/
+
 package com.trivago.triava.tcache.action;
 
 import javax.cache.event.EventType;
@@ -9,9 +25,11 @@ import com.trivago.triava.tcache.eviction.Cache;
 public class PutAction<K,V,W> extends Action<K,V,W>
 {
 
-	public PutAction(K key, V value, EventType eventType, Cache<K,V> actionContext)
+	final boolean countStatistics;
+	public PutAction(K key, V value, EventType eventType, Cache<K,V> actionContext, boolean countStatistics)
 	{
 		super(key, value, eventType, actionContext);
+		this.countStatistics = false;
 	}
 
 	@Override
@@ -37,7 +55,8 @@ public class PutAction<K,V,W> extends Action<K,V,W>
 	@Override
 	void statisticsImpl()
 	{
-		stats.incrementPutCount();
+		if (countStatistics)
+			stats.incrementPutCount();
 	}
 
 }
