a09743736895839d6c114c4619e28cd6f9e4a830
Christian Esken <Christian.Esken@trivago.com>
2017-01-04 14:05:42 +0100

    SAE-95 Document TCacheHolder class
    - Rename methods
    - Deprecate old methods
    - Document methods
    - Make it Serializable

--- src/main/java/com/trivago/triava/tcache/eviction/TCacheHolder.java
+++ src/main/java/com/trivago/triava/tcache/eviction/TCacheHolder.java
@@ -16,19 +16,69 @@
 
 package com.trivago.triava.tcache.eviction;
 
+import java.io.Serializable;
+
 /**
- * TODO Document this class
+ * Represents a Cache value with associated metadata like use count and access count.
+ * 
  * @author cesken
- *
- * @param <V> The value class
+
+ * @param <V> The value type
  */
-public interface TCacheHolder<V>
+public interface TCacheHolder<V> extends Serializable
 {
+	/**
+	 * Returns the value, that this holder holds. <b>Statistics effects</b>: Updates the access time and the use count.
+	 * @return The value
+	 */
 	V get();
+	
+	/**
+	 * Returns the value, that this holder holds. This method differs from {@link #get()} in that peek has no side effects on entry statistics.
+	 * @return The value
+	 */
 	V peek();
+	/**
+	 * @deprecated Use {@link #getLastAccessTime()}
+	 * @return See {@link #getLastAccessTime()}
+	 */
 	long getLastAccess();
+	
+	/**
+	 * Returns the last access time, given in MILLISECONDS since EPOCH
+	 * @return the last access time
+	 */
+	long getLastAccessTime();
+	
+	/**
+	 * Returns the number of times this holder was accessed.
+	 * @return the use count
+	 */
 	int getUseCount();
+	
+	/**
+	 * @deprecated Use {@link #getCreationTime()}
+	 * @return The creation time in ms
+	 */
 	long getInputDate(); // TODO rename This is not a Date! it is a timestamp
-	long getExpirationTime(); // TODO rename / remove
-	boolean isInvalid(); // TODO Revert to isValid()
+	
+	/**
+	 * Returns the creation time of this holder, given in ms since EPOCH.
+	 * The creation time represents the time the holder was put into the cache. Any put including replacing an existing entry
+	 * will create a new holder with the current time as creation time.  
+	 * @return The creation time in ms
+	 */
+	long getCreationTime(); 
+	
+	/**
+	 * Returns the expiration time, given in ms since EPOCH.
+	 * @return The expiration time in ms
+	 */
+	long getExpirationTime();
+	
+	/**
+	 * Returns whether this holder is valid.
+	 * @return true if it is not valid.
+	 */
+	boolean isInvalid();
 }
